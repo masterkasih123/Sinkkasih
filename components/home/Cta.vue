1e1f3574c35724d5b5fa36ebee81aed15fc3d959
dor123official <139676185+dor123official@users.noreply.github.com>
2024-08-06 05:39:06 +0700

    Update Cta.vue

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -3,14 +3,14 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      Deployment immediately
+      The Best Shortener Link
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
-      With just a few simple clicks, you can start deploying without any expenses.
+      Created with Love
     </p>
     <div class="flex mt-5">
       <HomeLink
-        href="https://github.com/ccbikai/sink?tab=readme-ov-file#%EF%B8%8F-deployment"
+        href="/"
         type="inverted"
         title="Start Deploy"
       >
